0108b2ca2327abaee9af843832102e75dc8f796c
Joohoon Kim <jayjhkim7@gmail.com>
2021-02-27 18:47:49 -0500

    initial commit

---
+++ greetMe/index.js
@@ -0,0 +1,24 @@
+const moment = require('moment');
+
+const greeting = {
+  en: 'hello',
+  hi: 'namaste',
+  sp: 'hola',
+  jp: 'konichiwa',
+  cn: 'nihao',
+};
+
+exports.handler = async (event) => {
+  let name = event.pathParameters.name;
+  let { lang, ...info } = event.queryStringParameters;
+  let message = `${greeting[lang] ? greeting[lang] : greeting['en']} ${name}`;
+  let response = {
+    message,
+    info,
+    timestamp: moment().unix(),
+  };
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+};
